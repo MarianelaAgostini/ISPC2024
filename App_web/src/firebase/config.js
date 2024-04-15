db1a4002d813e6592c8070f0fd3c0d7dd51a46c5
Fabio <fabiomarioluna@gmail.com>
2024-04-15 08:49:52 -0300

    subo pryecto web 2022

---
+++ App_web/src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCmUONPceaIvAx6QHwy2tw202Zx4ZbIvRM",
+   authDomain: "proyecto-web-coctelis.firebaseapp.com",
+   projectId: "proyecto-web-coctelis",
+   storageBucket:"proyecto-web-coctelis.appspot.com",
+   messagingSenderId: "901935873318",
+   appId: "1:901935873318:web:efbbffcf7fedcb290095e7"
+ };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
